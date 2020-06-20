e6c084f637a9d540739c2ed942658c1eef5024b3
KostasSar <kostassar8@gmail.com>
2020-06-20 22:01:53 +0300

    Switch from bye flood to ring-busy

--- bye-flood/byespam.py
+++ bye-flood/byespam.py
@@ -1,4 +1,13 @@
-#!/usr/bin/env python
-# -*- coding: utf-8 -*-
-from pwn import *
+from scapy.all import *
+
+BUSY_1 = 'SIP/2.0 486 Busy Here'
+BUSY_2 = 'X-Asterisk-HangupCause: Call Rejected'
+BUSY_3 = 'X-Asterisk-HangupCauseCode: 21'
+
+ringing = ''
+
+def pkt_callback(pkt):
+    pkt.show() # debug statement
+
+sniff(iface="wlp7s0", prn=pkt_callback, filter="udp", store=0, )
 
