03c849fbd36c38ce9e7ae4f2ceaa1fecf6266170
KostasSar <kostassar8@gmail.com>
2020-06-21 21:35:11 +0300

    no work yet

--- bye-flood/byespam.py
+++ bye-flood/byespam.py
@@ -1,8 +1,5 @@
 from scapy.all import *
-# import sip
-
-
-ringing = ''
+import binascii
 
 def pkt_callback(pkt):
     BUSY_1 = 'SIP/2.0 486 Busy Here'
@@ -10,7 +7,11 @@ def pkt_callback(pkt):
     BUSY_3 = 'X-Asterisk-HangupCauseCode: 21'
 
     ringing = pkt # debug statement
-    print(ringing.summary())
+    payload = ringing[3]
+    # print(type(payload))
+    payload = binascii.hexlify(bytes(payload))
+    print(payload.decode('hex'))
+
 
 sniff(iface="wlp7s0", prn=pkt_callback, filter="udp and port 5060", store=0)
 
